9d9acc34b87f6c0e808a58cefdb16e1f51d54fed
Hyperspace018 <hyperspace018@gmail.com>
2018-05-11 10:03:06 -0400

    Add new action for put new state to homework

--- app/Dashboard/actions.js
+++ app/Dashboard/actions.js
@@ -3,7 +3,8 @@ import { AsyncStorage } from 'react-native';
 
 const allOngoingHomeWork = (objectId) => {
 
-  const url = "https://api.backendless.com/9C2E4015-1686-CE6C-FFE9-C0980E239D00/C06C6410-D772-8A54-FFF2-A566229EC300/data/Homeworks?where=owner%3D'"+objectId+"'%20AND%20state%3D'ongoing'";
+  const url = "https://api.backendless.com/9C2E4015-1686-CE6C-FFE9-C0980E239D00/C06C6410-D772-8A54-FFF2-A566229EC300/data/Homeworks?where=owner%3D'"+objectId+"'%20AND%20state%3D'ongoing'&sortBy=created%20desc";
+
   return{
     type: "ALL_HOMEWORK_ONGOING",
     payload: axios({
@@ -16,7 +17,7 @@ const allOngoingHomeWork = (objectId) => {
 
 const allDoneHomework = (objectId) => {
 
-  const url = "https://api.backendless.com/9C2E4015-1686-CE6C-FFE9-C0980E239D00/C06C6410-D772-8A54-FFF2-A566229EC300/data/Homeworks?where=owner%3D'"+objectId+"'%20AND%20state%3D'done'";
+  const url = "https://api.backendless.com/9C2E4015-1686-CE6C-FFE9-C0980E239D00/C06C6410-D772-8A54-FFF2-A566229EC300/data/Homeworks?where=owner%3D'"+objectId+"'%20AND%20state%3D'done'&sortBy=created%20desc";
 
   return{
     type: "ALL_HOMEWORK_DONE",
@@ -30,7 +31,7 @@ const allDoneHomework = (objectId) => {
 
 const allPendingHomework = (objectId) => {
   
-  const url = "https://api.backendless.com/9C2E4015-1686-CE6C-FFE9-C0980E239D00/C06C6410-D772-8A54-FFF2-A566229EC300/data/Homeworks?where=owner%3D'"+objectId+"'%20AND%20state%3D'pending'";
+  const url = "https://api.backendless.com/9C2E4015-1686-CE6C-FFE9-C0980E239D00/C06C6410-D772-8A54-FFF2-A566229EC300/data/Homeworks?where=owner%3D'"+objectId+"'%20AND%20state%3D'pending'&sortBy=created%20desc";
 
   return{
     type: "ALL_HOMEWORK_PEND",
@@ -57,9 +58,28 @@ const postOngoingHomeWork = (value) => {
 
 }
 
+const putState = (objectId, state) => {
+  
+  const url = "https://api.backendless.com/9C2E4015-1686-CE6C-FFE9-C0980E239D00/C06C6410-D772-8A54-FFF2-A566229EC300/data/Homeworks/"+objectId;
+  
+  return{
+    type: "PUT_DONE",
+    payload: axios({
+      method: "PUT",
+      url: url,
+      data:{
+        "state": state
+      }
+    })
+  }
+
+}
+
+
 export{
   allOngoingHomeWork,
   allDoneHomework,
   allPendingHomework,
-  postOngoingHomeWork
+  postOngoingHomeWork,
+  putState
 }
